8e28b829a8d919be3417bc77efd83c2826a1fa6b
Himi <99322263+himi1024@users.noreply.github.com>
2024-11-14 17:35:03 +0800

    api testing with curl-pod(Minikube Environment)

--- cicd-main/cicd-main/main.py
+++ cicd-main/cicd-main/main.py
@@ -1,20 +1,22 @@
 from flask import Flask, jsonify, request
-from helloWorld import say_hello 
 
 app = Flask(__name__)
- 
+
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'  
+    return 'Flask Dockerized'
 
-@app.route('/call-script', methods=['GET'])
-def call_script():
-    # Call the function from helloWorld.py
-    message = say_hello()
-    return jsonify(message=message)  # Return the message as JSON
+@app.route('/api/hello', methods=['POST'])
+def api_hello():
+    data = request.get_json()  # Get JSON data from the request
+    if data and 'message' in data and data['message'] == 'hello':
+        return jsonify(message='hello world')  # Return hello world
+    else:
+        return jsonify(error='Invalid request'), 400  # Return error message
 
- 
+@app.route('/api/greet', methods=['GET'])
+def api_greet():
+    return jsonify(greeting='Hello, welcome to the API!')  # Return a greeting message
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888)
-
-    
